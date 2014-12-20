553f1dd9c6117f423b1f812d9200f70fa6869277
Kevin Allen <kma1660@gmail.com>
2014-12-20 21:43:04 +0300

    finished prototype simplified API, starts without error, not tested

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',require('./routes/dozer/index.js'));
-require('./routes/dozer/socket.js')(server);
+//require('./routes/dozer/socket.js')(server);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- routes/dozer/api.js
+++ routes/dozer/api.js
@@ -7,8 +7,8 @@ var sch = mon.Schema;
 var games = con.model ('games', new sch({
   name: String,
   description: String,
-  game: Array,
-  calc: Array,
+  game: [{name:String,type:String}],
+  calc: [{name:String,elements:[{name:String,worth:Number}]}],
   submissions: [new sch({
     match: String,
     team: Number,
@@ -16,8 +16,8 @@ var games = con.model ('games', new sch({
   })]
 }));
 
-router.route('/game/:id')
-  .get('/sub/:s', function (req,res) { //gets match with given id
+router.route('/game/:id/sub/:s')
+  .get(function (req,res) { //gets match with given id
     games.findById(req.params.id,function(err,x) {
       if (err) res.status(500).send(err);
       else {
@@ -27,66 +27,82 @@ router.route('/game/:id')
       }
     });
   })
-  .post('/sub', function (req,res) { //add match
+  .put(function (req,res) { //edit one match
     games.findById(req.params.id, function (err,x) {
       if (err) res.status(500).send(err);
       else {
-        x.submissions.create(req.body);
-        s = x.submissions[x.submissions.length-1];
-        x.save(function (err) {
-          if (err) res.status(500).send(err);
-          else res.send(s);
-        });
-      }
-    });
-  })
-  .put('/sub/:s', function (req,res) { //edit one match
-    games.findById(req.params.id, function (err,x) {
-      if (err) res.status(500).send(err);
-      else {
-        if (x.submissions.id(s)) {
-          x.submissions.id(s) = req.body;
+        var e = undefined;
+        (x.submissions.id(s) || e) = req.body;
+        if (e) res.send ("Error: Submission not found");
+        else {
           x.save(function (err,x) {
             if (err) res.status(500).send(err);
             else res.send(req.body);
           });
         }
-        else res.status(500).send(err);
       }
+    });
   })
-  .delete('/sub/:s', function (req,res) {
-    frc.matches.findByIdAndRemove(req.params.id,function(err,x) {
+  .delete(function (req,res) {
+    games.findById(req.params.id,function(err,x) {
       if (err) res.status(500).send(err);
       else {
-        frc.io.emit('delMatch',x._id);
+        var remove = x.submissions.id(s);
+        console.log(remove);
+        res.send('Not implemented yet');
+      }
+    });
+  });
+
+router.post('/game/:id/sub', function (req,res) { //add match
+  games.findById(req.params.id, function (err,x) {
+    if (err) res.status(500).send(err);
+    else {
+      y = x.submissions.push(req.body);
+      x.save(function (err) {
+        if (err) res.status(500).send(err);
+        else res.send(y);
+      });
+    }
+  });
+});
+
+router.route('/game/:id')
+  .get(function (req,res) { //get game with givin id,
+    games.findById(req.params.id, function (err,x) {
+      if (err) res.status(500).send(err);
+      else {
+        //x.submissions=null;
         res.send(x);
       }
     });
   })
-  .get('/', function (req,res) { //get game with givin id
-    games.findById(req.params.id, function (err,x) {
-      if (err) res.status(500).send(err);
-      else res.send(x);
-    });
-  })
-  .post('/', function (req,res) { //create new game
+  .post(function (req,res) { //create new game
+    req.body.submissions=[];
     games.create(req.body,function(err,x) {
       if (err) res.status(500).send(err);
       else res.send(x);
     });
   })
-  .put('/', function (req,res) { //edit game with id
-    games.findByIdAndUpdate(req.params.id,req.body,function(err,x) {
+  .put(function (req,res) { //edit game with id
+    games.findById(req.params.id,function(err,x) {
       if (err) res.status(500).send(err);
       else {
-        res.send(x);
+        y = x.submissions;
+        x = req.body;
+        x.submissions = y;
+        x.save(function (err,x) {
+          if (err) res.status(500).send (err);
+          else res.send(x);
+        });
       }
     });
   })
-  .delete('/', function (req,res) { //delete game with id
+  .delete(function (req,res) { //delete game with id
     games.findByIdAndRemove(req.params.id,function(err,x) {
       if (err) res.status(500).send(err);
       else res.send(x);
     });
   });
+
 module.exports = router;
