c26a88750f7b025f094e58864654779cc457cfe8
Kevin Allen <kma1660@gmail.com>
2014-12-21 08:26:54 +0300

    Created demo game if default game not found, made so I can switch between branches and still work with DB

--- routes/dozer/vars.js
+++ routes/dozer/vars.js
@@ -13,7 +13,16 @@ out.matchesSchema = new sch({
 });
 out.game = '5483a9f3ceeb864d701f713d';
 out.games.findById(out.game, function (err,x) { //finds default game and sets to current game
-  if (err) console.log(err);
+  if (err || !x) {
+    out.games.create({
+      name:'Demo',
+      game:[{name:'Goals',type:'Number'},{name:'Comment',type:'String'},{name:'Legit',type:'Boolean'}],
+      calc:[{name:'Total',elements:[{name:'Goals',worth:10},{name:'Legit',worth:50}]}]
+    },function (err,y) {
+      out.game = y._id;
+      out.matches = con.model(y.name,out.matchesSchema);
+    });
+  }
   else {
     out.matches = con.model(x.name,out.matchesSchema);
   }
