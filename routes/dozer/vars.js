d3111eba2b396eab0b833e25df169b7cd2edcb9e
Kevin Allen <kma1660@gmail.com>
2015-01-05 03:46:23 +0300

    finished merge

--- routes/dozer/vars.js
+++ routes/dozer/vars.js
@@ -16,29 +16,6 @@ var games = con.model ('FRCgames', new sch({
   })],
   teams: [new sch({team:{type:Number,unique:true},notes:String,name:String})]
 }));
-<<<<<<< HEAD
-out.matchesSchema = new sch({
-  team: Number,
-  elements: Object
-});
-out.game = '5483a9f3ceeb864d701f713d';
-out.games.findById(out.game, function (err,x) { //finds default game and sets to current game
-  if (err || !x) {
-    out.games.create({
-      name:'Demo',
-      game:[{name:'Goals',type:'Number'},{name:'Comment',type:'String'},{name:'Legit',type:'Boolean'}],
-      calc:[{name:'Total',elements:[{name:'Goals',worth:10},{name:'Legit',worth:50}]}]
-    },function (err,y) {
-      out.game = y._id;
-      out.matches = con.model(y.name,out.matchesSchema);
-    });
-  }
-  else {
-    out.matches = con.model(x.name,out.matchesSchema);
-  }
-});
-module.exports = out;
-=======
 var users = con.model('users', new sch({
   username: { type: String, required: true, unique: true},
   password: { type: String, required: true },
@@ -50,4 +27,3 @@ module.exports = {
   games: games,
   io: undefined
 };
->>>>>>> 0df780bd5812027d8f7df1daa863c1f0a1be1e34
